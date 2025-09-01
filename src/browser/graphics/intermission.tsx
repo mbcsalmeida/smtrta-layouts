2cf4d798ec969b892ea57afdbf1af157aeab539c
mbcsalmeida <mbcsalmeida@gmail.com>
2025-09-01 11:39:38 +0200

    accounting for races

--- src/browser/graphics/intermission.tsx
+++ src/browser/graphics/intermission.tsx
@@ -15,6 +15,7 @@ const LayoutContainer = styled.div`
 `;
 
 const IntermissionRunInfo = styled.div`
+  width: 700px;
   position: fixed;
   text-align: right;
   display: flex;
@@ -22,6 +23,15 @@ const IntermissionRunInfo = styled.div`
   line-height: 3.5rem;
   align-items: end !important;
 `;
+
+const InfoContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+	width: 734px;
+	height: 100%;
+	position: fixed;
+	right: 0;
+	top: 0;
+}`
 
 const IntermissionCurrentRunInfo = styled(IntermissionRunInfo)`
   top: 200px;
@@ -47,6 +57,15 @@ const MusicText = styled.h2`
   font-size: 33px;
 `
 
+function MultipleRunners(props){
+  const teams = props.teams;
+  const names = [];
+  const name = teams.map((team) => (
+    names.push(team.players[0].name)
+  ))
+    return names.join(" vs ");
+}
+
 const Intermission = () => {
   const currentRun = useCurrentRun();
   const nextRun = useNextRun();
@@ -54,65 +73,74 @@ const Intermission = () => {
   return (
     <ThemeProvider>
       <LayoutContainer>
-        <HeaderText $distance={128} className='shadow'>Coming Up</HeaderText>
-        {currentRun && (
-          <IntermissionCurrentRunInfo>
-            <AutoTextSize
-              mode="box"
-              className="shadow"
-              maxFontSizePx={48}>
-              {currentRun.game}
-            </AutoTextSize>
-            <span
-              className="shadow"
-              style={{
-                fontSize: "36px"
-              }}
-            >
-              {currentRun.category}
-            </span>
-            <span
-              className="shadow"
-              style={{
-                fontSize: "36px"
-              }}
-            >
-              {currentRun.teams[0]?.players[0]?.name}
-            </span>
-          </IntermissionCurrentRunInfo>
-        )}
-        {nextRun && (
-          <>
-            <HeaderText $distance={460} className='shadow'>On Deck</HeaderText>
-            <IntermissionNextRunInfo>
+        <MusicText>Deja Vu - Majin Tensei 2 Spiral Nemesis</MusicText>
+        <InfoContainer>
+          <HeaderText $distance={128} className='shadow'>Coming Up</HeaderText>
+          {currentRun && (
+            <IntermissionCurrentRunInfo>
               <AutoTextSize
                 mode="box"
                 className="shadow"
-                maxFontSizePx={40}>
-                {nextRun.game}
+                maxFontSizePx={48}>
+                {currentRun.game}
               </AutoTextSize>
               <span
                 className="shadow"
                 style={{
-                  fontSize: "30px"
+                  fontSize: "36px"
                 }}
               >
-                {nextRun.category}
+                {currentRun.category}
               </span>
               <span
-                className="shadow"
-                style={{
-                  fontSize: "30px"
-                }}
-              >
-                {nextRun.teams[0]?.players[0]?.name}
-              </span>
-            </IntermissionNextRunInfo>
-          </>
+                  className="shadow"
+                  style={{
+                    fontSize: "30px"
+                  }}
+                >
+                  {(currentRun.teams.length ?? 1) > 1 ?
+                    <MultipleRunners teams={currentRun.teams}/> : 
+                    currentRun.teams[0]?.players[0]?.name
+                }
+                </span>
+            </IntermissionCurrentRunInfo>
+          )}
+          {nextRun && (
+            <>
+              <HeaderText $distance={460} className='shadow'>On Deck</HeaderText>
+              <IntermissionNextRunInfo>
+                <AutoTextSize
+                  mode="box"
+                  className="shadow"
+                  maxFontSizePx={40}>
+                  {nextRun.game}
+                </AutoTextSize>
+                <span
+                  className="shadow"
+                  style={{
+                    fontSize: "30px"
+                  }}
+                >
+                  {nextRun.category}
+                </span>
+                <span
+                  className="shadow"
+                  style={{
+                    fontSize: "30px"
+                  }}
+                >
+                  {(nextRun.teams.length ?? 1) > 1 ?
+                    <MultipleRunners teams={nextRun.teams}/> : 
+                    nextRun.teams[0]?.players[0]?.name
+                }
+                </span>
+              </IntermissionNextRunInfo>
+            </>
 
-        )}
-        <MusicText>Deja Vu - Majin Tensei 2 Spiral Nemesis</MusicText>
-      </LayoutContainer>  
+          )}
+        </InfoContainer>
+
+      </LayoutContainer>
     </ThemeProvider>
   );
 };
